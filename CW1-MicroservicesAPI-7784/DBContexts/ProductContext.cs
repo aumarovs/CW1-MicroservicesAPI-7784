43d5f45d83ec288ce12de1677a6e065acc1ef53a
UAzam <umarovvv01@gmail.com>
2021-11-09 00:33:34 +0500

    Product and Category models added. DBContexts folder created and added ProductContext file in it.

---
+++ CW1-MicroservicesAPI-7784/DBContexts/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using CW1_MicroservicesAPI_7784.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW1_MicroservicesAPI_7784.DBContexts
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ CW1-MicroservicesAPI-7784/Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW1_MicroservicesAPI_7784.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ProductCategoryId { get; set; }
+        public Category ProductCategory { get; set; }
+        public decimal Price { get; set; }
+    }
+}
