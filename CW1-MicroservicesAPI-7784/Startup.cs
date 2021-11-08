9543f04cd8709e732d30fe710cf01331b248e35e
UAzam <umarovvv01@gmail.com>
2021-11-09 00:48:28 +0500

    Connection string added and made some changes in startup

--- CW1-MicroservicesAPI-7784/Startup.cs
+++ CW1-MicroservicesAPI-7784/Startup.cs
@@ -1,7 +1,9 @@
+using CW1_MicroservicesAPI_7784.DBContexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +28,10 @@ namespace CW1_MicroservicesAPI_7784
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddDbContext<ProductContext>(o => o.UseSqlServer(Configuration.GetConnectionString("ProductDB")));
 
-            services.AddControllers();
+            //services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CW1_MicroservicesAPI_7784", Version = "v1" });
